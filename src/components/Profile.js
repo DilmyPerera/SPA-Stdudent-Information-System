8cf2bb02c82bf31dac2a11f619d3b89b5e30feac
DilmyPerera <samadiniperera20@gmail.com>
2024-11-18 12:34:41 +0530

    profile layout and code is done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,10 +4,26 @@ export default function Profile(props){
             <table>
                 <tr>
                     <td rowSpan={7}>
-                    <img className="profile" src={require("../components/assests/profilepic/1.jpg")} alt="ProfilePicture"/>
+                    <img className="profile" src={require("../components/assests/profilepic/"+props.stu.profilePic)} alt="ProfilePicture"/>
                     </td>
                     <td>ID {props.stu.studentId} </td>
                 </tr>
+                <tr>
+                    <td>Full name :{props.stu.firstName + " " + props.stu.lastName}</td>
+                </tr>
+                <tr>
+                    <td>Age :{props.stu.age}</td>
+                </tr>
+                <tr>
+                    <td>Course :{props.stu.course}</td>
+                </tr>
+                <tr>
+                    <td>Address :{props.stu.address.city + " " + props.stu.address.country}</td>
+                </tr>
+                <tr>
+                    <td>Skills :{props.stu.skills.map(skill=>skill+=",")}</td>
+                </tr>
+
             </table>
         </div>
     );
